4a785ddb4168da1b4be226a451d779caabf18bf1
Romanov <evgeny.romanov@kpigroups.com>
2022-03-09 19:21:44 +0200

    fix

--- interceptor_jpa_hibernate/src/main/java/com/example/springconsole/interceptor/JpaInterceptor.java
+++ interceptor_jpa_hibernate/src/main/java/com/example/springconsole/interceptor/JpaInterceptor.java
@@ -2,36 +2,45 @@ package com.example.springconsole.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.EmptyInterceptor;
+import org.springframework.context.annotation.Scope;
 
 import javax.persistence.Column;
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.HashMap;
 
 @Slf4j
+@Scope("singleton")
 public class JpaInterceptor extends EmptyInterceptor {
     private boolean isMandaten;
     private static final String MANDATEN = "name";
+    private HashMap<String, Boolean> classCashe = new HashMap<>();
 
     @Override
     public Object getEntity(String entityName, Serializable id) {
         boolean mandatenTrigger = false;
-        try {
-            Class cls = Class.forName(entityName);
-            for (Field f : cls.getDeclaredFields()) {
-                Column column = null;
-                Annotation[] as = f.getAnnotations();
-                for (Annotation a : as) {
-                    if (a.annotationType() == Column.class)
-                        column = (Column) a;
-                }
+        if (classCashe.containsKey(entityName)) {
+            mandatenTrigger = classCashe.get(entityName);
+        } else {
+            try {
+                Class cls = Class.forName(entityName);
+                for (Field f : cls.getDeclaredFields()) {
+                    Column column = null;
+                    Annotation[] as = f.getAnnotations();
+                    for (Annotation a : as) {
+                        if (a.annotationType() == Column.class)
+                            column = (Column) a;
+                    }
 
-                if (column != null && column.name().equals(MANDATEN)) {
-                    mandatenTrigger = true;
-                    break;
+                    if (column != null && column.name().equals(MANDATEN)) {
+                        mandatenTrigger = true;
+                        break;
+                    }
                 }
+            } catch (Exception throwables) {
             }
-        } catch (Exception throwables) {
+            classCashe.put(entityName, mandatenTrigger);
         }
 
         if (mandatenTrigger && !isMandaten) {
